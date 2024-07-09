a6123a31d138b19bde4ec005f95406aa8663cea4
mannumishra <mannumishra2207@outlook.com>
2024-07-09 11:09:02 +0530

    add chnages

--- Utils/cloundanry.js
+++ Utils/cloundanry.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2
 
 cloudinary.config({
-    cloud_name: "dsimn9z1r",
-    api_key: "998919427255124", 
-    api_secret: "h-PsVovtSvzakWubj1X8sXJEtp4"
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
 })
 
 const uploadCloundanary = async (file) => {
@@ -16,5 +16,5 @@ const uploadCloundanary = async (file) => {
 }
 
 module.exports = {
-    uploadCloundanary:uploadCloundanary
+    uploadCloundanary: uploadCloundanary
 }
